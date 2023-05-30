02f48546bc396914f55c2cd260bd44fa3ea073da
huihyeon06 <agnea0829@gmail.com>
2023-05-31 02:09:11 +0900

    테스트

--- php와 MySQL연동/connect.php
+++ php와 MySQL연동/connect.php
@@ -1,5 +1,5 @@
 <?php
-$server = "localhost:3306";
+$server = "localhost3306";
 $user = "root";
 $password = "0000";
 $dbname = "Local instance MySQL80";
@@ -8,6 +8,4 @@ $conn = new mysqli($server, $user, $password, $dbname);
 
 if($conn->connect_error)
     echo "<h2>접속에 실패하였습니다.</h2>";
-else
-    echo "<h2>접속에 성공하였습니다.</h2>";
 ?>
