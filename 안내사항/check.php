aa6cc62aaecb3e6e103c6cce52c2fd2c12558ac1
huihyeon06 <agnea0829@gmail.com>
2023-06-06 02:33:46 +0900

    [Chore] : 사용자 이름 text에 name추가

--- 안내사항/check.php
+++ 안내사항/check.php
@@ -3,7 +3,7 @@
 include_once("connect.php");
 
 // form 데이터 읽어오기
-$user_name = $_POST["input_name"];
+$user_name = $_POST["name"];
 
-echo "<h3>사용자의 이름은 {$user_name}입니다."
+echo "<h3>사용자의 이름은 {$name}입니다."
 ?>
