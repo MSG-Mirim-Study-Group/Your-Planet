6658659132a7182f08d9add4adf8a6fcebfb5b2f
mic050r <103114387+mic050r@users.noreply.github.com>
2023-06-14 05:07:15 +0900

    [Add] : mbti에 따라 페이지 이동

--- 질문화면/js/mbti.js
+++ 질문화면/js/mbti.js
@@ -34,3 +34,55 @@ if (p > j) {
 }
 
 console.log("결과 : " + mbti);
+
+
+switch (mbti) {
+  case "infp":
+    window.location.href = "https://example.com";
+    break;
+  case "infj":
+    window.location.href = "https://example.com";
+    break;
+  case "intj":
+    window.location.href = "https://example.com";
+    break;
+  case "intp":
+    window.location.href = "https://example.com";
+    break;
+  case "isfp":
+    window.location.href = "https://example.com";
+    break;
+  case "isfj":
+    window.location.href = "https://example.com";
+    break;
+  case "istp":
+    window.location.href = "https://example.com";
+    break;
+  case "istj":
+    window.location.href = "https://example.com";
+    break;
+  case "enfp":
+    window.location.href = "https://example.com";
+    break;
+  case "enfj":
+    window.location.href = "https://example.com";
+    break;
+  case "entj":
+    window.location.href = "https://example.com";
+    break;
+  case "entp":
+    window.location.href = "https://example.com";
+    break;
+  case "esfj":
+    window.location.href = "https://example.com";
+    break;
+  case "estj":
+    window.location.href = "https://example.com";
+    break;
+  case "esfp":
+    window.location.href = "https://example.com";
+    break;
+  case "esfj":
+    window.location.href = "https://example.com";
+    break;
+}
