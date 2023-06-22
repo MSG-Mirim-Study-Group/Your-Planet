cb2dd638f274c27b79c0cdcbdfa8d68d50e7e525
haeunNoh06 <okhagrace06@naver.com>
2023-06-22 18:47:15 +0900

    [fix] : 오류 수정

--- result/connect.php
+++ result/connect.php
@@ -4,6 +4,6 @@ $id = "urplanet";
 $password = "sjdmlgodtjddms20@#";
 $dbname = "urplanet";
 
-$conn = mysqli_connect($host, $id, $password, $dbname, 3306);
+$conn = mysqli_connect($host, $id, $password, $dbname);
 
 ?>
